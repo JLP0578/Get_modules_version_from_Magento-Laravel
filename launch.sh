79ca21102fd1649072313360bc0b722877c78bab
Julie LP <jlp@ham05.com>
2024-05-30 16:39:39 +0200

    🏗️: Up main script

--- launch.sh
+++ launch.sh
@@ -6,14 +6,14 @@ function afficher_aide {
     echo "Usage: $0 [options]"
     echo "Options:"
     echo "  -h, --help           Obtenir de l'aide sur les commandes"
-    echo "  -m, --magento        Récupération des modules pour le projet Magento"
     echo "  -l, --laravel        Récupération des modules pour le projet Laravel"
     echo "  -s, --script         Récupération des modules pour les Scripts"
     echo "  --dev                Utilisation du BO dev"
+    echo "  --by-cron            Utilisation de l'option -d"
 }
 
-function recup_module_magento {
-    echo "Lancement de la récupération des modules pour le projet Magento..."
+function recup_module_script {
+    echo "Lancement de la récupération des modules pour le Script..."
 
     option="-it"
     if [[ "$by_cron" == true ]]; then
@@ -25,8 +25,36 @@ function recup_module_magento {
         argument=$DEV_ARGUMENT
     fi
 
-    echo "[Execute Docker] : $MAGENTO_NAME"
-    docker run --rm --name $MAGENTO_NAME -e CONTAINER_NAME=$MAGENTO_NAME --network $NETWORK -v $MAGENTO_LOCALCOMPOSERJSON:$CONTAINERCOMPOSERJSON:$READONLY -v $MAGENTO_LOCALCOMPOSERLOCK:$CONTAINERCOMPOSERLOCK:$READONLY -v $MAGENTO_LOCALAUTHJSON:$CONTAINERAUTHJSON:$READONLY -v $LOCALPROCESSSH:$CONTAINERPROCESSSH -v $LOCALENV:$CONTAINERENV $option $IMAGE sh -c "$argument"
+    
+    # Boucle pour tester les variables SCRIPT_NAME_1 à SCRIPT_NAME_10
+    for i in {1..10}; do
+        # Récupérer le nom de la variable
+        script_name_var="SCRIPT_NAME_$i"
+        script_package_json_var="SCRIPT_LOCALSCRIPTJSON_$i"
+        script_package_lock_json_var="SCRIPT_LOCALSCRIPTLOCK_$i"
+        
+        # Récupérer la valeur de la variable SCRIPT_NAME
+        script_name_value="${!script_name_var}"
+        script_package_json_value="${!script_package_json_var}"
+        script_package_lock_json_value="${!script_package_lock_json_var}"
+
+        # Vérifier si l'une des variables est vide, si oui, sortir de la boucle
+        if [ -z "$script_name_value" ] || [ -z "$script_package_json_value" ] || [ -z "$script_package_lock_json_value" ]; then
+            echo "Il y a plus de Script a récupérer. Arrêt de la boucle."
+            break
+        fi
+
+        # Exécuter la commande Docker
+        echo "[Execute Docker] : $script_name_value"
+        docker run --rm --name $script_name_value -e CONTAINER_NAME=$script_name_value --network $NETWORK -v $script_package_json_value:$CONTAINERPACKAGEJSON:$READONLY -v $script_package_lock_json_value:$CONTAINERPACKAGELOCK:$READONLY -v $LOCALPROCESSSH:$CONTAINERPROCESSSH -v $LOCALENV:$CONTAINERENV -it $IMAGE sh -c "$argument"
+
+        # Vérifier que le processus Docker précédent s'est terminé avant de continuer
+        docker_exit_code=$?
+        if [ $docker_exit_code -ne 0 ]; then
+            echo "Erreur : Docker a échoué avec le code de sortie $docker_exit_code. Arrêt de la boucle."
+            break
+        fi
+    done
 }
 
 function recup_module_laravel {
@@ -43,55 +71,10 @@ function recup_module_laravel {
     fi
 
     echo "[Execute Docker] : $LARAVEL_NAME"
-    docker run --rm --name $LARAVEL_NAME -e CONTAINER_NAME=$LARAVEL_NAME --network $NETWORK -v $LARAVEL_LOCALCOMPOSERJSON:$CONTAINERCOMPOSERJSON:$READONLY -v $LARAVEL_LOCALCOMPOSERLOCK:$CONTAINERCOMPOSERLOCK:$READONLY -v $LOCALPROCESSSH:$CONTAINERPROCESSSH -v $LOCALENV:$CONTAINERENV $option $IMAGE sh -c "$argument"
-}
-
-function recup_module_script {
-    echo "Lancement de la récupération des modules pour les Scripts..."
-
-    option="-it"
-    if [[ "$by_cron" == true ]]; then
-        option="-d"
-    fi
-
-    argument=$PROD_ARGUMENT
-    if [[ "$dev" == true ]]; then
-        argument=$DEV_ARGUMENT
-    fi
-
-    # Boucle pour tester les variables SCRIPT_NAME_1 à SCRIPT_NAME_10
-    for i in {1..10}; do
-        # Récupérer le nom de la variable
-        script_name_var="SCRIPT_NAME_$i"
-        script_composer_json_var="SCRIPT_LOCALSCRIPTJSON_$i"
-        script_composer_lock_var="SCRIPT_LOCALSCRIPTLOCK_$i"
-        
-        # Récupérer la valeur de la variable SCRIPT_NAME
-        script_name_value="${!script_name_var}"
-        script_composer_json_value="${!script_composer_json_var}"
-        script_composer_lock_value="${!script_composer_lock_var}"
-
-        # Vérifier si l'une des variables est vide, si oui, sortir de la boucle
-        if [ -z "$script_name_value" ] || [ -z "$script_composer_json_value" ] || [ -z "$script_composer_lock_value" ]; then
-            echo "Il y a plus de Script a récupérer. Arrêt de la boucle."
-            break
-        fi
-
-        # Exécuter la commande Docker
-        echo "[Execute Docker] : $script_name_value"
-        docker run --rm --name $script_name_value -e CONTAINER_NAME=$script_name_value --network $NETWORK -v $script_composer_json_value:$CONTAINERCOMPOSERJSON:$READONLY -v $script_composer_lock_value:$CONTAINERCOMPOSERLOCK:$READONLY -v $LOCALPROCESSSH:$CONTAINERPROCESSSH -v $LOCALENV:$CONTAINERENV $option $IMAGE sh -c "$argument"
-
-        # Vérifier que le processus Docker précédent s'est terminé avant de continuer
-        docker_exit_code=$?
-        if [ $docker_exit_code -ne 0 ]; then
-            echo "Erreur : Docker a échoué avec le code de sortie $docker_exit_code. Arrêt de la boucle."
-            break
-        fi
-    done
+    docker run --rm --name $LARAVEL_NAME -e CONTAINER_NAME=$LARAVEL_NAME --network $NETWORK -v $LARAVEL_LOCALPACKAGEJSON:$CONTAINERPACKAGEJSON:$READONLY -v $LARAVEL_LOCALPACKAGELOCK:$CONTAINERPACKAGELOCK:$READONLY -v $LOCALPROCESSSH:$CONTAINERPROCESSSH -v $LOCALENV:$CONTAINERENV $option $IMAGE sh -c "$argument"
 }
 
 function main {
-    local magento=false
     local laravel=false
     local script=false
     local dev=false
@@ -103,9 +86,6 @@ function main {
             -h|--help)
                 afficher_aide
                 exit 0
-                ;;
-            -m|--magento)
-                magento=true
                 ;;
             -l|--laravel)
                 laravel=true
@@ -128,10 +108,6 @@ function main {
         shift
     done
 
-    if [[ "$magento" == true ]]; then
-        recup_module_magento "$dev" "$by_cron"
-    fi
-
     if [[ "$laravel" == true ]]; then
         recup_module_laravel "$dev" "$by_cron"
     fi
@@ -140,7 +116,7 @@ function main {
         recup_module_script "$dev" "$by_cron"
     fi
 
-    if [[ "$magento" == false && "$laravel" == false && "$script" == false ]]; then
+    if [[ "$laravel" == false && "$script" == false ]]; then
         echo "Aucune option spécifiée. Utilisez -h ou --help pour afficher l'aide."
         afficher_aide
         exit 1
--- process.sh
+++ process.sh
@@ -21,7 +21,7 @@ function check {
             token=$DEV_TOKEN_ERRORMONITORING
         fi
 
-        body="{\"task\": \"$2'_composer'\", \"action\": \"$3\", \"error_log\": \"$EXITCODE\"}"
+        body="{\"task\": \"$2'_npm'\", \"action\": \"$3\", \"error_log\": \"$EXITCODE\"}"
 
         #Appel curl à l'API errorMonitoring
         curl -vvv -X POST -H "Authorization: Bearer "$token -H 'Content-Type: application/json' -H "Host: $host" -d "$body" --insecure $destination_url
@@ -30,7 +30,124 @@ function check {
     fi
 }
 
-function recup_module {
+# Fonction pour extraire la version npm spécifiée dans le package.json
+function get_npm_major_version() {
+    local package_json="package.json"
+    local npm_version=$(sed -n 's/.*"npm": "\([0-9]\+\).*/\1/p' "$package_json")
+    echo "$npm_version"
+}
+
+# Fonction pour fusionner trois chaînes JSON en un seul tableau JSON
+function merge_json_strings() {
+    # Vérifier si le nombre d'arguments est correct
+    if [ $# -ne 3 ]; then
+        echo "Usage: merge_json_strings <json_string1> <json_string2> <json_string3>"
+        return 1
+    fi
+    
+    # Concaténer les chaînes JSON avec une virgule entre elles
+    local merged_json="[{${1:1:-1},${2:1:-1},${3:1:-1}}]"
+
+    # Retourner le JSON fusionné
+    echo "$merged_json"
+}
+
+# Fonction pour extraire les parties de la version
+function extract_version_parts() {
+    local version="$1"
+    echo "$version" | tr '.' ' ' 
+}
+
+# Fonction pour comparer les versions et déterminer le type de mise à jour
+function compare_versions() {
+    local current_version="$1"
+    local new_version="$2"
+
+    local current_parts=($(extract_version_parts "$current_version"))
+    local new_parts=($(extract_version_parts "$new_version"))
+
+    # Déterminer le nombre de parties dans les versions
+    local num_parts_current=${#current_parts[@]}
+    local num_parts_new=${#new_parts[@]}
+    
+    # Comparaison pour déterminer le type de mise à jour
+    if [ "$num_parts_current" -eq "$num_parts_new" ]; then
+        local i
+        local update_type=""
+        for ((i=0; i<$num_parts_current; i++)); do
+            if [ "${current_parts[$i]}" -lt "${new_parts[$i]}" ]; then
+                case "$i" in
+                    $(($num_parts_current-1)))
+                        update_type="patch" ;;
+                    $(($num_parts_current-2)))
+                        update_type="minor" ;;
+                    $(($num_parts_current-3)))
+                        update_type="major" ;;
+                    *)
+                        update_type="unknown" ;;
+                esac
+                break
+            elif [ "${current_parts[$i]}" -gt "${new_parts[$i]}" ]; then
+                update_type="unknown"
+                break
+            fi
+        done
+        echo "$update_type"
+    else
+        echo "diff_parts"
+    fi
+}
+
+function format_before_send() {
+    # Initialisation de la chaîne JSON
+    json_string="{\"environnement\": \"$environment\", \"modules\": ["
+    # Utiliser jq pour parcourir chaque élément du tableau et extraire les informations
+    first=true
+    # Utiliser jq pour parcourir chaque élément du tableau et extraire les informations
+    for package_info in $(echo $merged_json | jq -c '.[] | to_entries[]'); do
+        name=$(echo $package_info | jq -r '.key')
+        homepage=$(echo $package_info | jq -r '.value.homepage')
+
+        current=$(echo $package_info | jq -r '.value.current')
+        # Si current est null, le remplacer par "0.0.0" (souvent pour les modules globaux)
+        if [ "$current" == "null" ]; then
+            current="0.0.0"
+        fi
+        wanted=$(echo $package_info | jq -r '.value.wanted')
+        latest=$(echo $package_info | jq -r '.value.latest')
+
+        update_type=$(compare_versions "$current" "$wanted")
+
+        # Si la version souhaitée est égale à la version actuelle, utiliser latest_update_type,
+        # sinon conserver update_type
+        if [ "$update_type" != "unknown" ]; then
+            severity=$(compare_versions "$current" "$latest")
+            if [ "$update_type" == "$latest_update_type" ]; then
+                update_type="$latest_update_type"
+            fi
+        fi
+
+        # Construire l'objet JSON pour chaque paquet
+        package_json=$(jq -n --arg name "$name" --arg current "$current" --arg wanted "$wanted" --arg latest "$latest" --arg severity "$severity" --arg homepage "$homepage" '{name: $name, current: $current, wanted: $wanted, latest: $latest, severity: $severity, homepage: $homepage}')
+
+        # Ajouter une virgule si ce n'est pas le premier objet
+        if [ "$first" = false ]; then
+            json_string="$json_string,"
+        fi
+        # Ajouter l'objet JSON au json_string
+        json_string="$json_string$package_json"
+
+        # Mettre à jour le flag first
+        first=false
+    done
+    # Fermer le tableau JSON
+    json_string="$json_string]}"
+
+    # Imprimer le json_string
+    echo "$json_string"
+}
+
+function recup_module { 
     destination_url='https://'$PROD_DOMAINE$PROD_URI_VERSIONING
     host=$PROD_DOMAINE
     token=$PROD_TOKEN_VERSIONING
@@ -45,42 +162,56 @@ function recup_module {
     check "$1" $CONTAINER_NAME $environment'_cd_app'
     echo "[Changement Dossier] : OK"
 
-    # Déclaration du tableau associatif
-    declare -A tableau_assoc
-    tableau_assoc["major"]="--major-only"
-    tableau_assoc["minor"]="--minor-only"
-    tableau_assoc["patch"]="--patch-only"
+    # Définir l'environnement
+    environment=$(grep -o '"name": *"[^"]*"' package.json | awk -F'"' '{print $4}')
+    check "$1" $CONTAINER_NAME 'npm_show_name'
+    echo "  [Récuppération Nom du Projet] : OK"
 
-    # Boucle à travers les clés du tableau associatif
-    for cle in "${!tableau_assoc[@]}"
-    do
-        echo "[$cle]";
+    # Récupérer la version majeure de npm spécifiée dans package.json
+    npm_major_version=$(get_npm_major_version)
 
-        # Définir l'environnement
-        environment=$(composer show -s --name-only)
-        check "$1" $CONTAINER_NAME $cle'_composer_show_name'
-        echo "  [Récuppération Nom du Projet] : OK"
-        
-        # Exécuter la commande composer outdated et stocker la sortie dans une variable
-        composer_output=$(composer outdated --ignore-platform-reqs --direct --strict --locked "${tableau_assoc[$cle]}" --no-interaction --no-plugins --no-scripts --no-cache --format json)
-        # Décommenter pour tester le check
-        # check "$1" $CONTAINER_NAME $cle'_composer_outdated'
-        echo "  [Récuppération Modules] : OK"
+    if [ -n "$npm_major_version" ]; then
+        echo "Version majeure de npm spécifiée dans package.json : $npm_major_version"
+        echo "Installation de la dernière version de npm dans la branche $npm_major_version.x"
+        npm install -g "npm@$npm_major_version"
+    else
+        echo "Aucune version majeure de npm spécifiée dans package.json. Installation de la dernière version de npm."
+        npm install -g npm@latest
+    fi
 
-        # Encapsuler le JSON modifié dans un nouvel objet JSON
-        json_output="{\"environnement\": \"$environment\", \"severity\": \"$cle\", \"modules\": $composer_output}"
-        echo "  [Traitement JSON] : OK"
+    # Récuppérer les modules globaux
+    module_globale=$(npm outdated -l -g -json)
+    module_globale=$(echo "$module_globale" | sed -e '/"dependent":/d' -e '/"type":/d' -e '/"location":/d')
+    echo "  [Récuppération Modules Globaux] : OK"
+    
+    # Récuppérer les modules dev
+    module_dev=$(npm outdated -l -json)
+    module_dev=$(echo "$module_dev" | sed -e '/"dependent":/d' -e '/"type":/d' -e '/"location":/d')
+    echo "  [Récuppération Modules] : OK"
 
-        # Envoyer la sortie via une requête curl (remplacez URL_DE_DESTINATION par l'URL de destination)
-        curl -vvv -X POST -H "Authorization: Bearer "$token -H 'Content-Type: application/json' -H "Host: $host" -d "$json_output" --insecure $destination_url 
-        check "$1" $CONTAINER_NAME $cle'_curl'
-        echo "  [Envoie CURL] : OK"
-    done
+    # Installation pour récuppérer les modules 
+    install=$(npm ci)
+    echo "  [Installation Modules] : OK"
+
+    # Récuppérer les modules
+    module=$(npm outdated -l -json)
+    module=$(echo "$module" | sed -e '/"dependent":/d' -e '/"type":/d' -e '/"location":/d')
+    echo "  [Récuppération Modules] : OK"
+
+    # Fusionner les objets JSON
+    merged_json=$(merge_json_strings "$module_globale" "$module_dev" "$module")
+    echo "[Merge Modules] : OK"
+
+    formatted_data=$(format_before_send "$merged_json" "$environment")
+    echo "[Format Datas] : OK"
+    
+    # Envoyer la sortie via une requête curl (remplacez URL_DE_DESTINATION par l'URL de destination)
+    curl -vvv -X POST -H "Authorization: Bearer "$token -H 'Content-Type: application/json' -H "Host: $host" -d "$formatted_data" --insecure $destination_url 
+    check "$1" $CONTAINER_NAME 'curl'
+    echo "  [Envoie CURL] : OK"
 }
 
 function main {
-    local magento=false
-    local laravel=false
     local dev=false
 
     # Vérifier les options passées
